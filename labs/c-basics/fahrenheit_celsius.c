87f215f93c7368936e5214371afe2d6f5efb99c1
Oscar Del Toro <oscar.deltoro@icloud.com>
2020-08-25 20:46:00 +0000

    Added argument capabilities to fahrenheit
    Signed-off-by: Oscar Del Toro <oscar.deltoro@icloud.com>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -6,12 +6,26 @@
 
 /* print Fahrenheit-Celsius table */
 
-int main()
-{
+int main(int argc, char **argv)
+{   
     int fahr;
+    if(argc==2){
+        sscanf (argv[1],"%d",&fahr);
+        printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
+    else if(argc==4){
+        int end,increment;
+        sscanf (argv[1],"%d",&fahr);
+        sscanf (argv[2],"%d",&end);
+        sscanf (argv[3],"%d",&increment);
+        for (fahr; fahr <= end; fahr = fahr + increment)
+	        printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+    }
+    else{
+        for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
 	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 
+    }
     return 0;
 }
