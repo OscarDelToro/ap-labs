29e5b784d849f47f8f8d3da5507fb1e1ea992b01
Oscar Del Toro <oscar.deltoro@icloud.com>
2020-09-01 19:55:53 +0000

    Solved word-reverser lab
    Signed-off-by: Oscar Del Toro <oscar.deltoro@icloud.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main(){
-    // Place your magic here
+    int c;
+    int buffer[3000];
+    int pointer=0;
+    do{
+        c=getchar();
+        buffer[pointer]=c;
+        if(c==10){//enter pressed
+            for(int i=1; i<=pointer; i++){
+                putchar(buffer[pointer-i]);
+            }
+            putchar(10);
+            pointer=0;
+            
+        }
+        else{
+            
+            pointer++;
+        }
+
+    }while(c!=EOF);
     return 0;
 }
