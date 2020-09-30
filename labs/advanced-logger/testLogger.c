abcd0a252f7fc5edf7d5fd3f16dca2eacc1dc257
Oscar Del Toro <oscar.deltoro@icloud.com>
2020-09-30 04:26:52 +0000

    Solved  advanced logger
    Signed-off-by: Oscar Del Toro <oscar.deltoro@icloud.com>

--- labs/advanced-logger/testLogger.c
+++ labs/advanced-logger/testLogger.c
@@ -3,33 +3,41 @@
 #include "logger.h"
 
 int main(int argc, char **argv){
+	if(argc>1){
+		printf("%d\n", atoi(argv[1]));
+		switch(atoi(argv[1])) {
+		case 1:
+		// default logging
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
+		case 2:
+		// stdout logging
+		initLogger("stdout");
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
+		case 3:
+		// syslog logging
+		initLogger("syslog");
+		infof("INFO Message %d", 1);
+		warnf("WARN Message %d", 2);
+		errorf("ERROR Message %d", 2);
+		break;
+		default:
+		errorf("Invalid test case");
+		break;
+		}
+	}
+	else{
+		errorf("Tester could not run because of lacking parameters.");
+	}
+	
 
-    printf("%d\n", atoi(argv[1]));
-    switch(atoi(argv[1])) {
-    case 1:
-	// default logging
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    case 2:
-	// stdout logging
-	initLogger("stdout");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    case 3:
-	// syslog logging
-	initLogger("syslog");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    default:
-	errorf("Invalid test case");
-	break;
-    }
+
+    
 
     return 0;
 }
