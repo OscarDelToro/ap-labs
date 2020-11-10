d9391ced876ff91636962c56876d3b74ff2e70d6
Oscar Del Toro <oscar.deltoro@icloud.com>
2020-11-10 00:39:11 +0000

    Solved web crawler
    Signed-off-by: Oscar Del Toro <oscar.deltoro@icloud.com>

--- labs/web-crawler/web-crawler.go
+++ labs/web-crawler/web-crawler.go
@@ -13,8 +13,8 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
 	"os"
 
 	"gopl.io/ch5/links"
@@ -23,45 +23,95 @@ import (
 //!+sema
 // tokens is a counting semaphore used to
 // enforce a limit of 20 concurrent requests.
+type Site struct {
+	currentDepth int
+	link         string
+}
+
 var tokens = make(chan struct{}, 20)
 
-func crawl(url string) []string {
-	fmt.Println(url)
-	tokens <- struct{}{} // acquire a token
-	list, err := links.Extract(url)
-	<-tokens // release the token
+var depth = flag.Int("depth", 1, "crawler depth")
+var fileName = flag.String("results", "default.txt", "result file")
 
+func crawl(website Site) []Site {
+	f, err := os.OpenFile(*fileName, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Print(err)
+		fmt.Println(err)
 	}
-	return list
+	_, err = fmt.Fprintln(f, website.link)
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+	}
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	if website.currentDepth < *depth {
+		tokens <- struct{}{} // acquire a token
+		list, err := links.Extract(website.link)
+		sites := make([]Site, 0)
+		for _, link := range list {
+			sites = append(sites, Site{link: link, currentDepth: website.currentDepth + 1})
+		}
+		<-tokens // release the token
+		if err != nil {
+			//log.Print(err)
+		}
+		return sites
+
+	}
+	return []Site{}
 }
 
 //!-sema
 
 //!+
 func main() {
-	worklist := make(chan []string)
+	flag.Parse()
+	if len(os.Args) < 4 {
+		fmt.Println("Wrong arguments")
+		return
+	}
+	worklist := make(chan []Site)
 	var n int // number of pending sends to worklist
 
 	// Start with the command-line arguments.
 	n++
-	go func() { worklist <- os.Args[1:] }()
+	fmt.Printf("crawling at %s\n", os.Args[3:])
+	file, err := os.Create(*fileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = file.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	go func() {
+		urls := make([]Site, 0)
+		urls = append(urls, Site{link: (os.Args[3:])[0], currentDepth: 0})
+		worklist <- urls
+	}()
 
 	// Crawl the web concurrently.
 	seen := make(map[string]bool)
 	for ; n > 0; n-- {
 		list := <-worklist
 		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
+			if !seen[link.link] {
+				seen[link.link] = true
 				n++
-				go func(link string) {
+				go func(link Site) {
 					worklist <- crawl(link)
 				}(link)
 			}
 		}
 	}
+	fmt.Printf("crawl finished, check results at %s \n", *fileName)
+
 }
 
 //!-
