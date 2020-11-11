55ba1c5dfda6a407e1776850e78afe34a28dd5c9
Oscar Del Toro <oscar.deltoro@icloud.com>
2020-11-11 00:37:02 +0000

    solved goroutines vs os threads
    Signed-off-by: Oscar Del Toro <oscar.deltoro@icloud.com>

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func main() {
-	var commsPerSecond int
+func ping(a chan uint, b chan uint) {
+	for {
+		a <- (<-b + 1)
+	}
+}
 
+func main() {
+	var commsPerSecond uint
+	a := make(chan uint)
+	b := make(chan uint)
+	var duration uint
+	duration = 7
+	fmt.Printf("Starting test. Running for %d seconds\n", duration)
+	go ping(a, b)
+	go ping(b, a)
+	b <- 0
+	time.Sleep(time.Duration(duration) * time.Second)
+	commsPerSecond = <-a / duration
 	fmt.Println("Communications Per Second : ", commsPerSecond)
+
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,49 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"time"
 )
+
+var output = make(chan int)
+var cont bool
+
+func pipeblock(a chan int, b chan int, counter int) {
+	if cont {
+		go pipeblock(b, a, counter+1)
+		b <- (<-a + 1)
+	} else {
+		output <- <-a
+	}
+
+}
 
 func main() {
 	var maxStages, transitTime int
+	maxmem := 68
+	a := make(chan int)
+	b := make(chan int)
+	cont = true
+	go pipeblock(a, b, 1)
+	startTime := time.Now()
+	a <- 0
 
+	var mem runtime.MemStats
+	for {
+		runtime.ReadMemStats(&mem)
+		if mem.HeapInuse/1024/1024 < uint64(maxmem) {
+			//fmt.Println(mem.HeapInuse / 1024 / 1024)
+
+		} else {
+			cont = false
+			break
+
+		}
+
+	}
+
+	maxStages = <-output
+	transitTime = int(time.Since(startTime).Milliseconds())
 	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+	fmt.Printf("Time to transit trough the pipeline :  %d milliseconds\n", transitTime)
 }
